5060612afad975d507fe0edb72941881a68f0dca
IKostarev <ilya.kostarev.9@gmail.com>
2022-06-15 10:24:19 +0500

    Add start server in main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+
+	"github.com/IKostarev/go-todo"
+)
+
 func main() {
-	
+	srv := new(todo.Server)
+	if err := srv.Run("8000"); err != nil {
+		log.Fatalf("Error start server is: %s", err.Error())
+	}	
 }
