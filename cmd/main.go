96e4945682ce58b512252651b5c6a0ae7f073159
IKostarev <ilya.kostarev.9@gmail.com>
2022-06-15 10:59:24 +0500

    подключение handler in main file

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,14 @@ import (
 	"log"
 
 	"github.com/IKostarev/go-todo"
+	"github.com/IKostarev/go-todo/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error start server is: %s", err.Error())
 	}	
 }
