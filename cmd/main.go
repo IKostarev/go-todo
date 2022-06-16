f4c27ced1cc8f3d983f081f426d7cbdf6713cf37
IKostarev <ilya.kostarev.9@gmail.com>
2022-06-16 14:16:34 +0500

    Create config file

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,27 @@ import (
 	"github.com/IKostarev/go-todo/pkg/handler"
 	"github.com/IKostarev/go-todo/pkg/repository"
 	"github.com/IKostarev/go-todo/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initCofig(); err != nil {
+		log.Fatalf("Error read config file is: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
-	handlers := handler.NewHandler(services)
+	handlers := handler.NewHandler(services) 
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error start server is: %s", err.Error())
 	}	
+}
+
+func initCofig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
