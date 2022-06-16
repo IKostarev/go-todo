c10821eac24195515ecf6c9b00331de6453cf871
IKostarev <ilya.kostarev.9@gmail.com>
2022-06-16 15:11:06 +0500

    Edit env

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 
 	"github.com/IKostarev/go-todo"
 	"github.com/IKostarev/go-todo/pkg/handler"
@@ -15,13 +19,17 @@ func main() {
 		log.Fatalf("Error read config file is: %s", err.Error())
 	}
 
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error load env: %s", err.Error())
+	}
+
 	db, err := repository.NewPostgresDB(repository.Config{
-		Host:     "localhost",
-		Port:     "5436",
-		Username: "postgres",
-		Password: "qwerty",
-		DBName:   "postgres",
-		SSLMode:  "disable",
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
 	})
 
 	if err != nil {
@@ -30,13 +38,13 @@ func main() {
 
 	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
-	handlers := handler.NewHandler(services) 
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	
+
 	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error start server is: %s", err.Error())
-	}	
+	}
 }
 
 func initCofig() error {
