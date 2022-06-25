b26ebd83859258f6bce7646eb1c8fcc6f10b60e9
IKostarev <ilya.kostarev.9@gmail.com>
2022-06-25 16:20:46 +0500

    Создание списков

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -3,11 +3,31 @@ package handler
 import (
 	"net/http"
 
+	"github.com/IKostarev/go-todo"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) createList(c *gin.Context) {
-	id, _ := c.Get(userCtx)
+	userid, err := getUserId(c)
+
+	if err != nil {
+		return
+	}
+
+	var input todo.TodoList
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := h.services.TodoList.Create(userid, input)
+
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
 	c.JSON(http.StatusOK, map[string]interface{}{
 		"id": id,
 	})
